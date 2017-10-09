0ea647a96977ea94e4f47390a994c1c23c14cad7
Tony Yip <tony@opensource.hk>
2017-10-10 00:03:03 +0800

    Add ga tracking

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 
+window.ga('create', 'UA-103662489-1', 'auto');
+
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
@@ -12,3 +14,4 @@ new Vue({
   el: '#app',
   render: h => h(App),
 });
+
--- src/router.js
+++ src/router.js
@@ -3,9 +3,17 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
     { path: '/', name: 'home', component: () => import('./pages/home/Page.vue') },
     { path: '/sla', name: 'sla', component: () => import('./pages/sla/Page.vue') },
   ],
 });
+
+router.beforeEach((to, form, next) => {
+  window.ga('set', 'page', `${window.location.host}${to.path}`);
+  window.ga('send', 'pageview');
+  next();
+});
+
+export default router;
